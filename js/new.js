d377c4abd14564e92ef0cb2b9f17ce48e0034668
ouederniAmine <amounaouederni@gmail.com>
2021-05-25 18:06:44 +0100

    up

--- js/new.js
+++ js/new.js
@@ -1,13 +1,1 @@
-window.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("title").innerHTML = sessionStorage.getItem("title");
-  document.getElementById("info").innerHTML =
-    sessionStorage.getItem("description");
-  document.getElementById("ProductPhotoImg").src =
-    sessionStorage.getItem("mainImg");
-    document.getElementById("ProductPhotoImg").src =
-    sessionStorage.getItem("mainImg");
-    document.getElementById("ProductPhotoImg").src =
-    sessionStorage.getItem("mainImg");
-    document.getElementById("ProductPhotoImg").src =
-    sessionStorage.getItem("mainImg");
-});
+
