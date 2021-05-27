e12f9eb108e1ee71539c2d4142b3fcd290bcbcc4
ouederniAmine <amounaouederni@gmail.com>
2021-05-27 13:21:13 +0100

    update

--- js/main.js
+++ js/main.js
@@ -47,26 +47,5 @@ function main() {
 }
 main();
 
-var Bilinga = {
-  title: "Bilinga",
-  description:
-    "Freshly cut Bilinga is yellow to orange-yellow, turning to an orange-red to golden brown colour. The 30-50 mm thick sapwood is clearly distinguishable and is pale yellow to white.<br> <br> Bilinga is listed in the Dutch norm NEN 5493 'Quality Requirements For Hardwoods In Road Construction Works, Hydraulic Engineering Works And Other Structural Applications’.<br>  <br>Bilinga is used for a diversity of construction purposes, for both inside and outside, fendering, jetties, bridges, bridge decks",
-  mainImg: "img/wood/BilingaMain.jpg",
-  img1: "imgwood\240px-Bilinga_kruisdraad.jpg",
-  img2: "imgwood\bilinga.jpeg",
-  img3: "imgwood\blinga-thumb.jpg",
-};
 
-$(function () {
-  if (document.getElementById("bilinga") != null) {
-    document.getElementById("bilinga").addEventListener("click", () => {
-      sessionStorage.setItem("title", Bilinga.title);
-      sessionStorage.setItem("description", Bilinga.description);
-      sessionStorage.setItem("mainImg", Bilinga.mainImg);
-      sessionStorage.setItem("img1", Bilinga.img1);
-      sessionStorage.setItem("img2", Bilinga.img2);
-      sessionStorage.setItem("img3", Bilinga.img3);
-      window.location.href = "product.html";
-    });
-  }
-});
+
